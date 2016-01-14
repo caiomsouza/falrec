d7019d0b782112d358f3fdbd8374caaf1e38f3d7
jtrecenti <julio.trecenti@gmail.com>
2016-01-14 09:34:45 -0200

    data atual no shiny

--- inst/shiny-examples/falrec/ui.R
+++ inst/shiny-examples/falrec/ui.R
@@ -3,12 +3,20 @@ library(shiny)
 fluidPage(
   sidebarLayout(
     sidebarPanel(
-      dateRangeInput(inputId='corte_temporal', label='Corte temporal', 
-                     min='2000-01-01', max='2014-10-01', 
-                     start='2008-01-01', end='2014-10-01', 
-                     format='mm/yyyy', separator='até', language='pt-BR'),
-      checkboxGroupInput('porte', label='Porte', 
-                         c('Micro'='micro', 'Médio'='med', 'Grande'='grande', 'Total'='total'),
-                         c('Micro'='micro', 'Médio'='med', 'Grande'='grande', 'Total'='total'))),
+      dateRangeInput(inputId = 'corte_temporal', 
+                     label = 'Corte temporal', 
+                     min = '2000-01-01', 
+                     max = lubridate::ceiling_date(lubridate::today(), 'month'), 
+                     start = '2008-01-01', 
+                     end = lubridate::ceiling_date(lubridate::today(), 'month'), 
+                     format = 'mm/yyyy', 
+                     separator = 'até', 
+                     language = 'pt-BR'),
+      checkboxGroupInput('porte', 
+                         label = 'Porte', 
+                         c('Micro' = 'micro', 'Médio' = 'med', 
+                           'Grande' = 'grande', 'Total' = 'total'),
+                         c('Micro' = 'micro', 'Médio' = 'med', 
+                           'Grande' = 'grande', 'Total' = 'total'))),
   mainPanel(plotOutput('grafico'))
 ))
